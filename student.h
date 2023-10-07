e4e1914e2f5542f95a6e6f53c328970abe94293a
Praveen S <praveenss2806@gmail.com>
2023-10-07 22:35:04 +0530

    char to string to student

--- student.h
+++ student.h
@@ -1,42 +1,47 @@
 #pragma once
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
+#include <string.h>
 #include "test.h"
 using namespace std;
 
-struct DETAIL{
-    char name[20];
-    char rollno[20];
+struct DETAIL
+{
+    string name;
+    string rollno;
 };
 
-class student{
+class student
+{
 private:
     DETAIL detail;
     fstream sfile;
     test exm;
+
 public:
     student();
 };
 
-student::student(){
-    cout<<"\t\t\t\t********Welcome Students********\n";
-    cout<<"\nPlease Enter your name and roll no to start the exam\n";
+student::student()
+{
+    cout << "\t\t\t\t********Welcome Students********\n";
+    cout << "\nPlease Enter your name and roll no to start the exam\n";
 
-    sfile.open("details.txt",ios::in|ios::out);
-    sfile.seekp(0L,ios::beg);
+    sfile.open("details.txt", ios::in | ios::out);
+    sfile.seekp(0L, ios::beg);
 
-    cout<<"\nEnter your name:";
+    cout << "\nEnter your name:";
     fflush(stdin);
-    gets(detail.name);
+    cin >> detail.name;
 
-    cout<<"\nEnter your rollno:";
+    cout << "\nEnter your rollno:";
     fflush(stdin);
-    gets(detail.rollno);
+    cin >> detail.rollno;
 
-    sfile.write((char *)&detail,sizeof(detail));
+    sfile.write((char *)&detail, sizeof(detail));
     sfile.close();
 
-    cout<<"\n\t\t\t\t******All the Best for the Exam******\n";
+    cout << "\n\t\t\t\t******All the Best for the Exam******\n";
 
     exm.start();
 }
